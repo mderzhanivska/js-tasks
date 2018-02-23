73d9e0a30c6d35a52f4b73620dbaa45bbf7b41ae
maria <masha@coderiver.com.ua>
2018-02-23 18:02:07 +0200

    [add]custom select page

--- src/js/app.js
+++ src/js/app.js
@@ -86,6 +86,62 @@ function openModal() {
 
 //custom select script start
 
+$('.select_city').each(function() {
+  let $this = $(this),
+    selectOption = $this.find('option'),
+    selectOptionLength = selectOption.length,
+    selectedOption = selectOption.filter(':selected'),
+    dur = 500;
+
+  $this.hide();
+  $this.wrap('<div class="select"></div>');
+  $('<div>', {
+    class: 'select__gap',
+    text: 'Город*'
+  }).insertAfter($this);
+
+  let selectGap = $this.next('.select__gap'),
+    caret = selectGap.find('.caret');
+  $('<ul>', {
+    class: 'select__list'
+  }).insertAfter(selectGap);
+
+  let selectList = selectGap.next('.select__list');
+  // Add li - option items
+  for (let i = 0; i < selectOptionLength; i++) {
+    $('<li>', {
+      class: 'select__item',
+      html: $('<span>', {
+        text: selectOption.eq(i).text()
+      })
+    })
+      .attr('data-value', selectOption.eq(i).val())
+      .appendTo(selectList);
+  }
+  let selectItem = selectList.find('li');
+
+  selectList.slideUp(0);
+  selectGap.on('click', function() {
+    if (!$(this).hasClass('on')) {
+      $(this).addClass('on');
+      selectList.slideDown(dur);
+
+      selectItem.on('click', function() {
+        let chooseItemIndex = $(this).index();
+        $($('#city').children()).removeAttr('selected');
+        $($('#city').children()[chooseItemIndex]).attr('selected', 'selected');
+        selectGap.text($(this).find('span').text());
+
+        selectList.slideUp(dur);
+        selectGap.removeClass('on');
+      });
+
+    } else {
+      $(this).removeClass('on');
+      selectList.slideUp(dur);
+    }
+  });
+});
 
 
 
